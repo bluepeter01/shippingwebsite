f0fb97e777dd56849093d912546481f96423f101
Josiah <carmenjosh84@gmail.com>
2025-06-08 19:20:46 +0100

    updated

--- src/lib/Div2.svelte
+++ src/lib/Div2.svelte
@@ -9,7 +9,7 @@
 		/>
 		<div>
 			<h1 class="text-4xl font-bold text-blue-500 md:w-[600px] md:text-5xl">
-				Reliable Shipping for Cargo and Parcels Shipment!!
+				Reliable for Cargo and Parcel Shipment
 			</h1>
 			<h3 class="mt-5 text-2xl">Trusted by Individuals and Businesses Worldwide</h3>
 			<div class="mt-2 w-[100px] border border-black"><hr /></div>
